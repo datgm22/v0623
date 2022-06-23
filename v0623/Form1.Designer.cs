55113d66a06a73bf71f7675b24904bb27bc42437
A601-00 <tnk.dat.class@gmail.com>
2022-06-23 14:02:04 +0900

    おまけ

--- v0623/Form1.Designer.cs
+++ v0623/Form1.Designer.cs
@@ -30,12 +30,56 @@ namespace v0623
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(184, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(・ω・)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SCORE 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(464, 321);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- v0623/Form1.cs
+++ v0623/Form1.cs
@@ -12,10 +12,45 @@ namespace v0623
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+        int score = 100;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+            }
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -vx;
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -vy;
+            }
+
+            score--;
+            label2.Text = $"SCORE {score}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            MessageBox.Show("たなか");
+        }
     }
 }
